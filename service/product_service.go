af5044b205cc9ce13b4664156bca0819ab4cb58a
Hélio Rodrigues <49084401+heliorodri@users.noreply.github.com>
2023-04-02 15:42:12 -0300

    Refactoring repository (#10)
    * debug config
    
    * refactoring
    
    * improve error messages
    
    ---------
    
    Co-authored-by: helio.rodrigues <5Aa7^NG%Pg5jv*p>

--- service/product_service.go
+++ service/product_service.go
@@ -3,6 +3,8 @@ package service
 import (
 	models "coffee-api-go/model"
 	"coffee-api-go/repository"
+	"errors"
+	"strconv"
 )
 
 type ProductService struct {
@@ -16,18 +18,36 @@ func NewProductService(repo *repository.ProductRepository) *ProductService {
 }
 
 func (s *ProductService) GetAllProducts() ([]*models.Product, error) {
-	return s.repo.GetAllProducts()
+	products, err := s.repo.GetAllProducts()
+
+	if err != nil {
+		return nil, errors.New("FAILED to get all products.\n Error: " + err.Error())
+	}
+
+	return products, nil
 }
 
-func (s *ProductService) GetProductByID(id int) (*models.Product, error) {
-	return s.repo.GetProductByID(id)
+func (s *ProductService) GetProductByID(id uint) (*models.Product, error) {
+	product, err := s.repo.GetProductByID(id)
+
+	if err != nil {
+		return nil, errors.New("FAILED to get product with id " + strconv.FormatUint(uint64(id), 10) + ".\n Error: " + err.Error())
+	}
+
+	return product, nil
 }
 
-func (s *ProductService) CreateProduct(p *models.Product) error {
-	return s.repo.CreateProduct(p)
+func (s *ProductService) CreateProduct(p *models.Product) (*models.Product, error) {
+	product, err := s.repo.CreateProduct(p)
+
+	if err != nil {
+		return nil, errors.New("FAILED to create product.\n Error: " + err.Error())
+	}
+
+	return product, nil
 }
 
-func (s *ProductService) UpdateProduct(p *models.Product) error {
+func (s *ProductService) UpdateProduct(p *models.Product) (*models.Product, error) {
 	return s.repo.UpdateProduct(p)
 }
 
