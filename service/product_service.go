3c53df185232d0dba31b5b3937145fc8e0f743e1
helio.rodrigues <5Aa7^NG%Pg5jv*p>
2023-04-02 00:16:37 -0300

    product service

---
+++ service/product_service.go
@@ -0,0 +1,36 @@
+package service
+
+import (
+	models "coffee-api-go/model"
+	"coffee-api-go/repository"
+)
+
+type ProductService struct {
+	repo repository.ProductRepository
+}
+
+func NewProductService(repo *repository.ProductRepository) *ProductService {
+	return &ProductService{
+		repo: *repo,
+	}
+}
+
+func (s *ProductService) GetAllProducts() ([]*models.Product, error) {
+	return s.repo.GetAllProducts()
+}
+
+func (s *ProductService) GetProductByID(id uint) (*models.Product, error) {
+	return s.repo.GetProductByID(id)
+}
+
+func (s *ProductService) CreateProduct(p *models.Product) error {
+	return s.repo.CreateProduct(p)
+}
+
+func (s *ProductService) UpdateProduct(p *models.Product) error {
+	return s.repo.UpdateProduct(p)
+}
+
+func (s *ProductService) DeleteProduct(id uint) error {
+	return s.repo.DeleteProduct(id)
+}
