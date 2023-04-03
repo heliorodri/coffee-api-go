0cbb63c79667389c30a720bd6e90b981c5931d94
Hélio Rodrigues <49084401+heliorodri@users.noreply.github.com>
2023-04-02 22:22:58 -0300

    Customer service unit test (#15)
    * getAll customers unit test
    
    * add unit tests for customer service
    
    * refactoring test logic
    
    ---------
    
    Co-authored-by: helio.rodrigues <5Aa7^NG%Pg5jv*p>

---
+++ service/customer/customer_service_test.go
@@ -0,0 +1,99 @@
+package service
+
+import (
+	"errors"
+	"reflect"
+	"testing"
+
+	models "coffee-api-go/model/customer"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func TestGetAllWithData(t *testing.T) {
+	expected := []*models.Customer{
+		{
+			Name:  "John Doe",
+			Email: "johndoe@mail.com",
+		},
+		{
+			Name:  "Jane Smith",
+			Email: "janesmith@mail.com",
+		},
+	}
+
+	repo := &mockRepository{
+		mockGetAll: func() ([]*models.Customer, error) {
+			return expected, nil
+		},
+	}
+
+	service := &CustomerService{repo: repo}
+	customers, err := service.GetAll()
+
+	if err != nil {
+		t.Errorf("Unexpected error: %v", err)
+	}
+
+	assert.True(t, reflect.DeepEqual(customers, expected))
+}
+
+func TestGetAllWithEmptyReturn(t *testing.T) {
+	expected := []*models.Customer{}
+
+	repo := &mockRepository{
+		mockGetAll: func() ([]*models.Customer, error) {
+			return expected, nil
+		},
+	}
+
+	service := &CustomerService{repo: repo}
+	customers, err := service.GetAll()
+
+	if err != nil {
+		t.Errorf("Unexpected error: %v", err)
+	}
+
+	assert.True(t, reflect.DeepEqual(customers, expected))
+}
+
+func TestGetAllWithError(t *testing.T) {
+	repo := &mockRepository{
+		mockGetAll: func() ([]*models.Customer, error) {
+			return nil, errors.New("Error connecting to database")
+		},
+	}
+
+	service := &CustomerService{repo: repo}
+	_, err := service.GetAll()
+
+	assert.EqualError(t, err, "FAILED to get all customers. Error: Error connecting to database")
+}
+
+type mockRepository struct {
+	mockGetAll  func() ([]*models.Customer, error)
+	mockGetByID func() (*models.Customer, error)
+	mockCreate  func() (*models.Customer, error)
+	mockUpdate  func() (*models.Customer, error)
+	mockDelete  func() error
+}
+
+func (repository *mockRepository) GetAll() ([]*models.Customer, error) {
+	return repository.mockGetAll()
+}
+
+func (repository *mockRepository) GetByID(id uint) (*models.Customer, error) {
+	return repository.mockGetByID()
+}
+
+func (repository *mockRepository) Create(p *models.Customer) (*models.Customer, error) {
+	return repository.mockCreate()
+}
+
+func (repository *mockRepository) Update(p *models.Customer) (*models.Customer, error) {
+	return repository.mockUpdate()
+}
+
+func (repository *mockRepository) Delete(id uint) error {
+	return repository.mockDelete()
+}
